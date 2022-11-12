06311d93ca052728cd70556dc0937949152d47cd
Rama Sanjaya <ramasanjaya2202@gmail.com>
2022-11-12 10:17:02 +0700

    menambahkan soal1

---
+++ Soal1.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+/*
+ * 1. meminta inputan nama1,nama2,jam
+ * 2. ubah jam int -> sapaan string
+ * 3. tampilkan ke layar
+ */
+
+//  string, int, double
+
+public class Soal1 {
+    public static void main(String[] args) {
+        // Scanner in = new Scanner(System.in);
+        // String nama1 = in.nextLine();
+        // String nama2 = in.nextLine();
+        // int jam = in.nextInt();
+
+        // sapaan(nama1, jam);
+        // sapaan(nama2, jam);
+        double angka = 3;
+        System.out.println((5 + 2) / 2 * angka);
+
+
+    }
+
+    // private static void sapaan(String nama, int jam) {
+    //     String sapaan = " ";
+    //     if (jam >= 6 && jam <= 11) {
+    //         sapaan = "pagi";
+    //     } else if (jam >= 12 && jam <= 14) {
+    //         sapaan = "Siang";
+    //     }
+
+    //     System.out.println("Halo, " + nama + ". Selamat " + sapaan + ".");
+    // }
+}
